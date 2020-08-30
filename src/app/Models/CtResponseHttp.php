12604478a246eab74b1c73a0c41d55f5034894d9
mayckol <mayckoll@live.com>
2020-08-30 16:42:26 -0300

    fix translate messages

--- src/app/Models/CtResponseHttp.php
+++ src/app/Models/CtResponseHttp.php
@@ -4,6 +4,8 @@
 namespace Mayckol\CtResponse\app\Models;
 
 
+use Illuminate\Support\Facades\Lang;
+
 class CtResponseHttp implements CtResponseInterface
 {
     /**
@@ -13,13 +15,18 @@ class CtResponseHttp implements CtResponseInterface
      */
     public function build(array $configResponses, ?array $runTimeData = [])
     {
+        $mergeData = array_merge($configResponses, $runTimeData);
+        if (array_key_exists('message', $mergeData)) {
+            foreach ($mergeData['message'] as &$message) {
+                $message = Lang::get($message);
+            }
+        }
         return redirect()->route(
-            $configResponses['redirect_to'], !empty($runTimeData['params']) ? $runTimeData['params'] : [],
+            $configResponses['redirect_to'],
+            !empty($runTimeData['params']) ? $runTimeData['params'] : [],
             !empty($configResponses['status']) ? $configResponses['status'] : 302,
             !empty($configResponses['headers']) ? $configResponses['headers'] : []
         )
-            ->with(
-                array_merge($configResponses, $runTimeData)
-            );
+            ->with($mergeData);
     }
 }
--- src/app/Models/CtResponseJson.php
+++ src/app/Models/CtResponseJson.php
@@ -5,6 +5,7 @@ namespace Mayckol\CtResponse\app\Models;
 
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Lang;
 
 class CtResponseJson implements CtResponseInterface
 {
@@ -15,8 +16,14 @@ class CtResponseJson implements CtResponseInterface
      */
     public function build(array $configResponses, ?array $runTimeData = [])
     {
+        $mergeData = array_merge($configResponses, $runTimeData);
+        if (array_key_exists('message', $mergeData)) {
+            foreach ($mergeData['message'] as &$message) {
+                $message = Lang::get($message);
+            }
+        }
         return response()->json(
-            array_merge($configResponses, $runTimeData),
+            $mergeData,
             !empty($runTimeData['status']) ? $runTimeData['status'] : 302,
             !empty($configResponses['headers']) ? $configResponses['headers'] : [],
             !empty($configResponses['options']) ? $configResponses['options'] : 0
